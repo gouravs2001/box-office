a8ce84fb25d9d6fcf654e743beb2154f988ea7fe
ourav Sharma <gourav27012001@gmail.com>
2022-09-10 16:59:41 +0530

    added loading and error div in show.js page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,12 +5,30 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=cast&embed[]=seasons`).then(result => {
-      setShow(result);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=cast&embed[]=seasons`)
+      .then(result => {
+        if (isMounted) {
+          setShow(result);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
-  console.log(show);
+
+  if (isLoading) return <div>Data is Loading</div>;
   return <div>Show</div>;
 };
 
