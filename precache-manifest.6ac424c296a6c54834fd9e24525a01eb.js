5b39c405c40102ec02e9de6b9d3bc79a65504732
ourav Sharma <gourav27012001@gmail.com>
2022-09-14 00:20:23 +0530

    Updates

---
+++ precache-manifest.6ac424c296a6c54834fd9e24525a01eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27ecfd18b065827c0852ff69d70e83a",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "3683e3ec8924c1243fc3",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "f73f2f2853c1410f3aaf",
+    "url": "/box-office/static/js/2.2b1a189b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.2b1a189b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed92057e0c6d406a1335",
+    "url": "/box-office/static/js/3.90d9a6d0.chunk.js"
+  },
+  {
+    "revision": "3683e3ec8924c1243fc3",
+    "url": "/box-office/static/js/main.f59f9255.chunk.js"
+  },
+  {
+    "revision": "b4501d0f99ed0b4d2af8",
+    "url": "/box-office/static/js/runtime-main.22fcb4a4.js"
+  }
+]);
